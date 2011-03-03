15e4a7793acdc172bbe56d95e2578db92b62daf4
thoms <thoms@4fc0fbde-e734-0410-b98b-b551705f41f8>
2011-03-03 21:23:26 +0000

    initial check in
    git-svn-id: https://forge.epn-campus.eu/svn/edna/trunk/rcp@3006 4fc0fbde-e734-0410-b98b-b551705f41f8

---
+++ org.edna.datamodel.generateds/src/org/edna/datamodel/generateds/JavaExtensions.java
@@ -0,0 +1,38 @@
+/*
+ *    Project: The EDNA Kernel
+ *             http://www.edna-site.org
+ *
+ *    File: "$Id:$"
+ *
+ *    Copyright (C) 2008-2009 European Synchrotron Radiation Facility
+ *                            Grenoble, France
+ *
+ *    Principal authors: Marie-Francoise Incardona (incardon@esrf.fr)
+ *                       Olof Svensson (svensson@esrf.fr)
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    and the GNU Lesser General Public License  along with this program.
+ *    If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.edna.datamodel.generateds;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class JavaExtensions {
+	public static String getTimestamp () {
+		SimpleDateFormat df = new SimpleDateFormat("EEE MMM d hh:mm::ss yyyy", Locale.ENGLISH);
+		return df.format(new Date());
+	}
+}
