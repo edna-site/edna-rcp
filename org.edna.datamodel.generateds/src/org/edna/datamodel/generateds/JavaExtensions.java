e2006ba11721ece9125a6a5293c941db89e89831
thoms <thoms@4fc0fbde-e734-0410-b98b-b551705f41f8>
2011-03-09 14:24:55 +0000

    Databindings generated from DSL datamodel now Added UI action to invoke the Databinding generation also for .edna_datamodel files
    git-svn-id: https://forge.epn-campus.eu/svn/edna/trunk/rcp@3052 4fc0fbde-e734-0410-b98b-b551705f41f8

--- org.edna.datamodel.generateds/src/org/edna/datamodel/generateds/JavaExtensions.java
+++ org.edna.datamodel.generateds/src/org/edna/datamodel/generateds/JavaExtensions.java
@@ -27,12 +27,43 @@
 package org.edna.datamodel.generateds;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import org.eclipse.emf.ecore.EObject;
+import org.edna.datamodel.datamodel.ComplexType;
 
 public class JavaExtensions {
 	public static String getTimestamp () {
 		SimpleDateFormat df = new SimpleDateFormat("EEE MMM d hh:mm::ss yyyy", Locale.ENGLISH);
 		return df.format(new Date());
+	}
+
+	/**
+	 * Returns the name of the resource the object is contained in without the trailing file extension.
+	 */
+	public static String getResourceName (EObject obj) {
+		if (obj.eResource()==null) return "";
+		final String resource = obj.eResource().getURI().lastSegment();
+		return resource.substring(0, resource.lastIndexOf('.'));
+	}
+
+	public static List<ComplexType> sortComplexTypes (List<ComplexType> types) {
+		List<ComplexType> result = new ArrayList<ComplexType>(types);
+		Collections.sort(result, new Comparator<ComplexType>() {
+			public int compare(ComplexType t1, ComplexType t2) {
+				if (t1 == t2) return 0;
+				if (t1.getBaseType()!=null && t1.getBaseType()==t2)
+					return 1;
+				if (t2.getBaseType()!=null && t2.getBaseType()==t1)
+					return -1;
+				return t1.getName().compareTo(t2.getName());
+			}
+		});
+		return result;
 	}
 }
